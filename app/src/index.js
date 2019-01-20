8fc8b471fa36716d95ee97a546c1e8757f954df2
thunkli <1280597658@qq.com>
2019-01-20 17:19:36 +0800

    img

--- app/src/index.js
+++ app/src/index.js
@@ -29,7 +29,7 @@ ipcRenderer.on('mainWindow-share', function(e, count, size) {
     window.shareSize = size;
 });
 $(document).on("click", ".ui-share", function(e) {
-    shell.openExternal('https://service.weibo.com/share/share.php?url=https://github.com/thunkli/hummingbird&ralateUid=1564456012&language=zh_cn&title=蜂鸟客户端已经帮我处理图片' + window.shareCount + '次，压缩空间' + (window.shareSize / (1024 * 1024)).toFixed(4) + 'M，好人一生平安&pic=https://raw.githubusercontent.com/thunkli/hummingbird/master/demo/hummingbird.jpg&searchPic=false&style=number');
+    shell.openExternal('https://service.weibo.com/share/share.php?url=https://github.com/thunkli/hummingbird&ralateUid=1564456012&language=zh_cn&title=蜂鸟客户端已经帮我处理图片' + window.shareCount + '次，压缩空间' + (window.shareSize / (1024 * 1024)).toFixed(4) + 'M，好人一生平安&pic=https://user-images.githubusercontent.com/1193966/51437209-258baa80-1cd6-11e9-87b1-10bb2115a49d.jpg&searchPic=false&style=number');
 });
 $(document).on("click", ".ui-minimized", function(e) {
     ipcRenderer.send('main-minimized');
